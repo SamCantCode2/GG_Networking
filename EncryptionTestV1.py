54ce341fb742b58684faae66e630bac2af0a29e0
Samhruth Ananthanarayanan <122143797+SamCantCode2@users.noreply.github.com>
2023-11-08 21:35:20 +0530

    Add files via upload

---
+++ EncryptionTestV1.py
@@ -0,0 +1,23 @@
+import PyEncry as pe
+import socket
+
+ip = socket.gethostbyname(socket.gethostname())
+port = 8008
+addr = (ip, port)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(addr)
+print(f"Connected at {addr[0]}/{addr[1]}")
+flag = True
+while flag:
+    msg = input("> ")
+    #encrypt here
+    client.send(msg.encode('utf-32'))
+    if msg == '-disc':
+        flag = False
+    else:
+        reply = client.recv(1024).decode('utf-32')
+        #decrypt here
+        print(f"> {reply}")
+client.close()
+
---
+++ TestServer.py
@@ -0,0 +1,34 @@
+import socket
+import threading
+import _thread
+import PyEncry as pe
+
+ip = socket.gethostbyname(socket.gethostname())
+port = 8008
+addr = (ip, port)
+dc = '-disc'
+
+def clientthread(conn, addr):
+    print(f"New connection: {addr[0]}/{addr[1]}")
+    flag = True
+    while flag:
+        msg = conn.recv(1024).decode('utf-32')
+        #decrypt here
+        if msg == dc:
+            flag = 0
+        print(f"[{addr}]: {msg}")
+        reply = input("Enter your reply: ")
+        #encrypt here
+        conn.send(reply.encode('utf-32'))
+    conn.close()
+
+print("Server is starting...")
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(addr)
+server.listen()
+print(f"Server is listening on {ip}/{port}")
+while True:
+    conn, addr = server.accept()
+    thread = threading.Thread(target=clientthread, args=(conn, addr))
+    thread.start()
+    print(f"Connection Count: {threading.activeCount() - 1}")
